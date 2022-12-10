571746749efe309167386f1ef21f43dfad6e1cd1
Aiden Fine <fineaiden@gmail.com>
2022-12-10 15:33:10 -0500

    Settings set to correct values...

--- main/main.py
+++ main/main.py
@@ -1,12 +1,28 @@
 import threading
 import cv2
 import imutils
+from cvzone.PoseModule import PoseDetector
+import cvzone
+from datetime import datetime
 
 cap = cv2.VideoCapture(0)
-first_frame = None
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
+#set img 
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# iii
+fpsReader = cvzone.FPS()
+detector = PoseDetector()
+
+# counter 
+count = 0
+motion = False
+
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+size = (width, height)
 
 _, start_frame = cap.read()
 start_frame = imutils.resize(start_frame, width=500)
@@ -18,52 +34,53 @@ alarm = False
 alarm_mode = False
 alarm_counter = 0
 
+# get date and time
+timestamp = datetime.now()
+dt_string = timestamp.strftime("%d-%m-%Y-%H-%M-%S")
+
+# save img
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter(f'/Users/aidenfine/security-system-with-alerts/security-system-with-alerts/main/output/{dt_string}.avi',fourcc, 20.0, size)
+
 def beep_alarm():
     global alarm
-    for _ in range(5):
+    for _ in range(20):
         if not alarm_mode:
             break
-        print('xxx')
+        print("!!!!!!!!!!!!!!!!!!!!!!!")
     alarm = False
 
 while True:
-    if first_frame is None:
-        first_frame=start_frame
-        continue
-    _, frame = cap.read()
+    success, frame = cap.read()
+
+    # detect human
+    frame = detector.findPose(frame)
+    mlist,bbox = detector.findPosition(frame)
     
     frame = imutils.resize(frame, width=500)
 
 
     
     if alarm_mode:
-        frame_bw = cv2.cvtColor(frame, cv2.COLOR)
+        frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame_bw = cv2.GaussianBlur(frame_bw, (5, 5), 0)
 
-        difference = cv2.absdiff(first_frame, start_frame)
+
+        difference = cv2.absdiff(frame_bw, start_frame)
         threshold = cv2.threshold(difference, 50,255, cv2.THRESH_BINARY)[1]
         threshold = cv2.dilate(threshold,None,iterations=2)
         start_frame = frame_bw
 
-            # delta_frame = cv2.absdiff(first_frame,gray)
-            # threshold_frame = cv2.threshold(delta_frame,50,255,cv2.THRESH_BINARY)[1]
-            # threshold_frame = cv2.dilate(threshold_frame,None,iterations=2)
-
-        # (cntr,_)=cv2.findContours(threshold.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-        # for contour in cntr:
-        #     if cv2.contourArea(contour)<1000:
-        #         continue
-        #     (x,y,w,h)=cv2.boundingRect(contour)
-        #     cv2.rectangle(frame,(x,y),(0,255,0),3)
-
-        if threshold.sum() > 300:
+        if threshold.sum() > 10000:
             alarm_counter += 1
+            print("MOTON ")
         else:
+            print("-------------------------------")
             if alarm_counter > 0:
-                alarm_counter -= 1
+                alarm_counter -= 1          
 
         cv2.imshow("Cam", threshold)
+        cv2.imshow("footage", frame)
         
     else:
         cv2.imshow("Cam", frame)
